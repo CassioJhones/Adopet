af84e3a60aa7f736fba43da09be1768e42ca3833
CASSIO <cassio.bjhones@gmail.com>
2023-11-06 19:05:50 -0300

    reflection

--- Alura.Adopet.Console/Help.cs
+++ Alura.Adopet.Console/Help.cs
@@ -1,12 +1,10 @@
 ﻿using System.Reflection;
-
 namespace Alura.Adopet.Console;
 
-
 [DocComando(instrucao: "help",
-            documentacao: "adopet help <parametro> ous simplemente adopet help comando que exibe informações de ajuda dos comandos.")]
-internal class Help{
-
+            documentacao: "adopet help <parametro> comando que exibe informações de ajuda dos comandos. \n adopet help <COMANDO> para acessar a ajuda de um comando especifico")]
+internal class Help
+{
     private Dictionary<string, DocComando> docs;
     public Help()
     {
@@ -14,28 +12,28 @@ internal class Help{
             Where(x => x.GetCustomAttributes<DocComando>().Any()).
             Select(x => x.GetCustomAttribute<DocComando>()!).ToDictionary(d => d.Instrucao);
     }
-    public void ExibeDocumentacao(string parametros){
-
+    public void ExibeDocumentacao(string[] parametros)
+    {
         System.Console.WriteLine("Lista de comandos.");
-        // se não passou mais nenhum argumento mostra help de todos os comandos
-        if (parametros.Length == 1){
+
+        if (parametros.Length == 1)
+        {
             System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help comando que exibe informações de ajuda dos comandos.");
             System.Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
             System.Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
             System.Console.WriteLine("Comando possíveis: ");
-            System.Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
-            System.Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado.\n\n\n\n");
             System.Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando.\n\n\n");
+
+            foreach (var doc in docs.Values)
+                System.Console.WriteLine(doc.Documentacao);
         }
-        else if (parametros.Length == 2){
-            string comandoaSerExibido = parametros;
-            if (comandoaSerExibido.Equals("import")){
-                System.Console.WriteLine(" adopet import <arquivo> " +
-                    "comando que realiza a importação do arquivo de pets.");
-            }
-            if (comandoaSerExibido.Equals("show")){
-                System.Console.WriteLine(" adopet show <arquivo>  comando que " +
-                    "exibe no terminal o conteúdo do arquivo importado.");
+        else if (parametros.Length == 2)
+        {
+            string comandoaSerExibido = parametros[1];
+            if (docs.ContainsKey(comandoaSerExibido))
+            {
+                var comando = docs[comandoaSerExibido];
+                System.Console.WriteLine(comando.Documentacao);
             }
         }
     }
